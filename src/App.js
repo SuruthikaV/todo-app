8f9a45a327d9c768d1a96ffeaece4770fd6ad0cc
commercial website <suruthikav98@gmail.com>
2026-08-03 16:23:54 +0530

    todo form done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ToDo from "./ToDo"
 
 function App() {
   return (
     <div className="App">
-      
+       <ToDo/>
     </div>
   );
 }
